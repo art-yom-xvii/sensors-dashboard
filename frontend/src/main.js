2d4afa47092821c577a68a26ba740f8cc68a6ceb
art-yom-xvii <artjoms.doilnicins@inbox.lv>
2026-03-04 13:36:50 +0200

    Add Ant Design Vue library

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/reset.css'
 import App from './App.vue'
 import './index.css'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(Antd).mount('#app');
